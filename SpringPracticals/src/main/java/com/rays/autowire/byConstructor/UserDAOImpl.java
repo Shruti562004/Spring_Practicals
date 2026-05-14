78735126236482421d3098ffb6eeb651eb5b8c25
Shruti <rathoreshruti672@gmail.com>
2026-05-14 23:36:28 +0530

    done

---
+++ SpringPracticals/src/main/java/com/rays/autowire/byConstructor/UserDAOImpl.java
@@ -0,0 +1,8 @@
+package com.rays.autowire.byConstructor;
+
+public class UserDAOImpl implements UserDAOInt {
+
+	public void add() {
+		System.out.println("add method..!");
+	}
+}
---
+++ SpringPracticals/src/main/java/com/rays/autowire/byType/UserDAOImpl.java
@@ -0,0 +1,9 @@
+package com.rays.autowire.byType;
+
+public class UserDAOImpl implements UserDAOInt {
+
+	public void add() {
+		System.out.println("add method..!");
+	}
+
+}
